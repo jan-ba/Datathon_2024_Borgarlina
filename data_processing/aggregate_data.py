aaec500cc735f5159882ee4d187ec522a57fbc9a
alexandersvafdal <alexandersvarfdal@gmail.com>
2024-11-23 12:28:51 +0000

    Added first columns the data

--- data_processing/aggregate_data.py
+++ data_processing/aggregate_data.py
@@ -1,5 +1,7 @@
 # Small area id: id of the small area
 # Density: current density of the small area
-# Income distribution: the distribution of income
-# Age distribution: 
-columns = ["smallAreaId", "density", "income_distribution", "age_distribution", "geometry", "projected_dwellings"]
+# Income distribution: the distribution of income in the small area per year (dictionary, keys: years, values: income distribution [buckets])
+# Age distribution: distibution of age in the small area (age buckets of 5 years starting at 0-4)
+# Geometry: the lat and long coordinates for the small area polygon
+# Projected dwellings: 
+columns = ["smallAreaId", "density", "income_distribution_per_year", "age_distribution", "geometry", "projected_dwellings"]
