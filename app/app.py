90c62084c2ad00a2fe124b473bf818b5ddbde006
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2024-11-24 01:17:22 +0000

    Clean-up

--- app/app.py
+++ app/app.py
@@ -28,8 +28,6 @@ def generateStops(year):
         points.append((point.y, point.x))
     return points
 
-bill_rng = (min(tips.total_bill), max(tips.total_bill))
-
 # Add page title and sidebar
 ui.page_opts(title="Borgarlínan", fillable=True)
 
@@ -49,16 +47,6 @@ with ui.sidebar(open="open"):
 
     ui.input_action_button("reset", "Reset filter")
 
-# Add main content
-ICONS = {
-    "user": fa.icon_svg("user", "regular"),
-    "wallet": fa.icon_svg("wallet"),
-    "currency-dollar": fa.icon_svg("dollar-sign"),
-    "ellipsis": fa.icon_svg("ellipsis"),
-}
-
-
-# MAP 
 with ui.layout_columns(col_widths=[8, 4]):
     with ui.card(full_screen=True):
         ui.card_header("Capital area")
@@ -71,52 +59,15 @@ with ui.layout_columns(col_widths=[8, 4]):
                 center=(64.11,-21.90), 
                 zoom=11.5)  
 
-    '''with ui.layout_column_wrap(width="250px"):
+    with ui.layout_column_wrap(width="250px"):
         with ui.card(full_screen=False):
             ui.card_header("Stop Data")
-            @render_widget  
-            def table1():
-                return render.DataTable(
-                    {},
-                    width="250px",
-                    height="100px"
-                )
-        
-        with ui.card(full_screen=False):
-            ui.card_header("Stop Data1")
-            @render_widget  
-            def table2():
-                return render.DataTable(
-                    {},
-                    width="250px",
-                    height="100px"
-                )
-        
-        with ui.card(full_screen=False):
-            ui.card_header("Stop Data2")
-            @render_widget  
-            def table3():
-                return render.DataTable(
-                    {},
-                    width="250px",
-                    height="100px"
-                )'''
-        
         
 ui.include_css(app_dir / "styles.css")
 
 # --------------------------------------------------------
 # Reactive calculations and effects
 # --------------------------------------------------------
-
-
-@reactive.calc
-def tips_data():
-    bill = input.total_bill()
-    idx1 = tips.total_bill.between(bill[0], bill[1])
-    idx2 = tips.time.isin(input.time())
-    return tips[idx1 & idx2]
-
 
 @reactive.effect
 def _():
